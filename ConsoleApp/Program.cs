d009dd1bc358c126af84807d5b5f38b27ee0e1b1
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-13 11:15:43 +0100

    Nullable

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -9,3 +9,44 @@ void DoSth()
 {
     Console.WriteLine("I am working...");
 }
+
+Nullable(null);
+
+void Nullable(string? ala)
+{
+    int? a = 0;
+    Nullable<int> b = null;
+    int c;
+
+    if(a - b == 0 || a - b == null)
+    {
+        /*if(a == null || b == null)
+            c = 0;
+        else*/
+            c = (a + b) ?? 0; // ?? - jeśli wynik po lewej jest null to użyj wartości po prawej stronie
+    }
+    else
+    {
+        int? result = a - b;
+
+        //if (result != null)
+        //if (result is not null)
+        /*if (result.HasValue)
+        {
+            c = result.Value;
+        }
+        else
+        {
+            c = 0;
+        }*/
+
+        c = result.HasValue ? result.Value : 0; // ? : - operator warunkowy:  (warunek) ? (tak) : (nie);
+    }
+
+
+    c = (a - b == 0 || a - b == null) ? ((a + b) ?? 0) : ((a - b).HasValue ? (a - b).Value : 0);
+
+    c = ((a - b == 0 || a - b == null) ? (a + b) : (a - b)) ?? 0;
+
+
+}
