4486e565b1800bbdb1c0ba7fefb3c5a586c6a8f2
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-13 15:27:45 +0100

    Lambda

---
+++ DelegatesApp/LambdaExample.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesApp
+{
+    internal class LambdaExample
+    {
+        Func<int, int, int> Calculator { get; set; }
+        Func<string> SomeFunc { get; set; }
+        Action<int> SomeAction { get; set; }
+        Action AnotherAction { get; set; }
+
+
+        public void Test()
+        {
+            Calculator += Plus;
+
+            Calculator += //delegate (int a, int b) { return a - b; };
+                          //(int a, int b) => { return a - b; };
+                          //(a, b) => { return a - b; };
+                            (a, b) => a - b;
+
+            SomeFunc += //delegate { return "Hello!"; };
+                        () => "Hello!";
+
+            SomeAction += //delegate (int a) { Console.WriteLine(a); };
+                            //(a) => Console.WriteLine(a);
+                         a => Console.WriteLine(a);
+
+            AnotherAction += //delegate { Console.WriteLine(); };
+                              () => Console.WriteLine();
+
+
+            SomeMethod(x => Console.WriteLine(x.ToUpper()), "Hello!");
+            SomeMethod(x => Console.WriteLine(x.ToLower()), "Hello!");
+        }
+
+        private int Plus(int x, int y)
+        { return x + y; }
+
+
+
+        void SomeMethod(Action<string> stringAction, string value)
+        {
+            stringAction?.Invoke(value);
+        }
+    }
+}
