98f855b24fb73810d911cdfc3f6dd5539b3baa55
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-13 14:04:39 +0100

    PeopleApp (I)

---
+++ PeopleApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using Models;
+using Services.InMemory;
+using Services.Interfaces;
+
+IPeopleService peopleService = new PeopleService();
+
+Person p = new Person("Ewa", "Ewowska", new DateTime(1990, 1, 1));
+peopleService.Create(p);
+
+p = new Person("Adam", "Adamski", new DateTime(1980, 12, 24));
+peopleService.Create(p);
+
+
+IEnumerable<Person> people = peopleService.Read();
+
+foreach(Person person in people)
+{
+    Console.WriteLine($"{person.Id}\t{person.FirstName}\t{person.LastName}\t{person.Age}");
+}
