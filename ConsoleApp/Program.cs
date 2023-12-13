b0e6215bf63dc3816c74eafbd3c29ad153b83e1c
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-13 10:28:09 +0100

    Instrukcje najwyższego poziomu

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace ConsoleApp
+﻿//Instrukcja najwyższego poziomu
+Console.WriteLine("Hello, World!");
+
+
+//Metody napisane jako instrukcje najwyższego poziomu nie posiadają modyfikatorów dostępu,
+//ponieważ kod w pliku z instrukcjami najwyższego poziomu zostanie otoczony metodą Main, a metody w metodzie nie mogą posiadać moedyfikatorów dostępu
+void DoSth()
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine("I am working...");
 }
---
+++ ConsoleApp/Program2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Class!");
+
+            void DoSth()
+            {
+                Console.WriteLine("I am working...");
+            }
+        }
+
+        public void DoSth()
+        {
+            Console.WriteLine("I am working...");
+        }
+    }
+}
