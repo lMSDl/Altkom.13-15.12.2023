3fb013e74a1b97c92d24b04f55117154bab21131
Paweł Łęczycki <pawel@msd.info.pl>
2023-12-14 09:27:53 +0100

    ProductsService

---
+++ Models/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Product
+    {
+        public Product() { }
+        public Product(string name, float price, DateTime? expirationdate)
+        {
+
+            Name = name;
+            Price = price;
+            ExpirationDate = expirationdate;
+        }
+        public Product(string name, float price) : this(name, price, null) { }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public DateTime? ExpirationDate { get; set; }
+        public string Summary
+        {
+            get
+            {
+                return $"{Name}: {Price}zł";
+            }
+        }
+    }
+}
---
+++ Services.InMemory/ProductsService.cs
@@ -0,0 +1,37 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.InMemory
+{
+    internal class ProductsService
+    {
+
+        private readonly ICollection<Product> _products = new List<Product>
+        {
+            new Product("Kawa", 12, DateTime.Now.AddSeconds(3261272)),
+            new Product("Herbata", 45, DateTime.Now.AddSeconds(2938381)),
+            new Product("Lalka", 223),
+            new Product("Książka", 93),
+            new Product("Mleko", 12.2f, DateTime.Now.AddSeconds(3919919)),
+            new Product("Czekolada", 9.99f, DateTime.Now.AddSeconds(2501001)),
+            new Product("Telefon", 2000.50f),
+            new Product("Miś", 222),
+            new Product("Kiełbasa", 45, DateTime.Now.AddSeconds(1500000)),
+            new Product("Ser", 35, DateTime.Now.AddSeconds(2033092)),
+            new Product("Spodnie", 123),
+            new Product("Bluza", 53.12f),
+            new Product("Buty", 90.70f),
+            new Product("Woda", 20, DateTime.Now.AddSeconds(6038329)),
+            new Product("Czapka", 50),
+            new Product("Zegarek", 550),
+            new Product("Szalik", 123.22f),
+            new Product("Rękawiczki", 76),
+            new Product("Olej", 49, DateTime.Now.AddSeconds(3000000)),
+            new Product("Choinka", 300)
+        };
+    }
+}
